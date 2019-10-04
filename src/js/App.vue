ccfd7d57a7c563e993150e061960ab516141fa79
Igor Stupka <igorstupka123@gmail.com>
2019-10-04 17:46:37 +0300

    font fix

--- src/js/App.vue
+++ src/js/App.vue
@@ -22,13 +22,8 @@ export default {
 </script>
 
 <style lang="sass">
-#app
-  font-family: 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-  color: #2c3e50
-  margin-top: 60px
-.active
-  background-color: green
+@import "../css/main"
+/*.active*/
+  /*background-color: green*/
 
 </style>
